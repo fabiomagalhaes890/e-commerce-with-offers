bfeb2f7bc72cfd30f7de4205c33d25ebe983c025
fabiomagalhaes890 <fabiomagalhaes890@gmail.com>
2023-04-12 09:20:26 -0300

    chore: fixing bug on docker-compose, added healthcheck to verify if database on before up application

--- angular/src/app/shared/shared.module.ts
+++ angular/src/app/shared/shared.module.ts
@@ -4,8 +4,6 @@ import { CartButtonComponent } from './cart-button/cart-button.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { ListComponent } from './list/list.component';
 
-
-
 @NgModule({
   declarations: [
     CartButtonComponent,
